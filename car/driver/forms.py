3564461dce830f912a8f8bd852b277080d7686f2
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-22 12:56:34 +0300

    Added driver user model

---
+++ car/driver/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import DriverProfile, CarPool, Location, Reviews
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
