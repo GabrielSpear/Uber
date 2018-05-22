e90f994fb5b07eb5cdc89675c51ac6ddff5fa0fa
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-22 10:30:43 +0300

    Added Home request function

--- car/passanger/views.py
+++ car/passanger/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.views.generic import View
+from django.contrib import messages
 
 # Create your views here.
+
+
+def home(request):
+
+    title = 'Passenger'
+    return render(request, 'passenger/home.html', {'title': title})
