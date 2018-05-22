5f19193f4a416f524d89ce3d799596c15edd8b02
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-22 10:33:55 +0300

    Added User profile, profile update and profile pic view function

--- car/passanger/views.py
+++ car/passanger/views.py
@@ -22,3 +22,24 @@ def profile(request, username):
 
     title = f"{user.username}"
     return render(request, 'passenger/profile.html', {"user": user, "profile": profile, "location": location})
+
+
+def update_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = PassengerProfileForm(
+            request.POST, instance=request.user.passengerprofile, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(
+                request, ('Your profile was successfully updated!'))
+            return HttpResponseRedirect("/passenger/passenger_profile/%s" % user.username)
+        else:
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = PassengerProfileForm(
+            instance=request.user.passengerprofile)
