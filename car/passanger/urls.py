680568a4905c2d4500feb6e70f1f5ad3aa62569b
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-22 12:09:41 +0300

    Added driver view url

--- car/passanger/urls.py
+++ car/passanger/urls.py
@@ -9,11 +9,4 @@ urlpatterns = [
         views.profile, name='profile'),
     url(r'^passenger_profile/(?P<username>[-_\w.]+)/update/$',
         views.update_profile, name='update_profile'),
-    url(r'^drivers/$', views.find_driver, name='find_driver'),
-    url(r'^driver/profile/(\d+)$', views.driver_profile, name='driver_profile'),
-    url(r'^driver/(\d+)/review/$', views.review_driver, name='review_driver'),
-    url(r'^driver/profile/comment/$', views.add_comment, name='comment'),
-    url(r'^location/(?P<username>[-_\w.]+)/update/$',
-        views.update_location, name='update_location_view'),
-    url(r'^pickup_locations/$', views.pickup_locations, name='pickup_locations'),
 ]
