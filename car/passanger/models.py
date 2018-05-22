fe04076fe44bd05d5a28bab926a6e220b4190fff
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-22 14:34:25 +0300

    Passanger location

--- car/passanger/models.py
+++ car/passanger/models.py
@@ -20,3 +20,32 @@ class PassengerProfile(models.Model):
     def getPic(self):
         if not self.profile_pic:
             return '/media/profile/driver/user.svg'
+
+    # link to in-built user model
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            PassengerProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.passengerprofile.save()
+    # end of link
+
+
+class Location(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='passenger_location')
+    current = models.CharField(max_length=100, blank=True)
+    destination = models.CharField(max_length=100, blank=True)
+
+    # link to in-built user model
+    @receiver(post_save, sender=User)
+    def create_user_location(sender, instance, created, **kwargs):
+        if created:
+            Location.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_location(sender, instance, **kwargs):
+        instance.passenger_location.save()
+    # end of link
