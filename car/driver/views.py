484cc2feb25060b61f1f2b0eee1238378f00bd8f
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-22 09:42:42 +0300

    Added driver and Passanger apps

---
+++ car/driver/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+# Create your views here.
+
+
+def home(request, username):
+    user = User.objects.get(username=username)
+    driver_profile = DriverProfile.objects.get(user=user)
+    carpool = CarPool.objects.filter(id=user.id)
+    num_passengers = len(carpool)
+    capacity = driver_profile.car_capacity
+    print(carpool)
+    print(num_passengers)
+    if request.method == 'POST':
+        carpool_form = CarPoolForm(request.POST)
+
+        if carpool_form.is_valid() and num_passengers <= capacity:
+            carpool_form.save()
+            messages.success(request, ('You have a ride with this driver!'))
+            return HttpResponseRedirect("/driver/%s" % user.username)
+        else:
+            messages.error(request, ('This car is full, try another driver.'))
+    else:
+        carpool_form = CarPoolForm()
