6c2238a0997afa8ec082d82e0c80a9e3b0549df4
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-22 13:06:26 +0300

    User prof pic,age and gender

--- car/passanger/forms.py
+++ car/passanger/forms.py
@@ -8,3 +8,9 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class PassengerProfileForm(forms.ModelForm):
+    class Meta:
+        model = PassengerProfile
+        fields = ['profile_pic', 'gender', 'age']
